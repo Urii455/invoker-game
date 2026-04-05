f5c92a526498c076b9bcf91b8ab7652f23230e7c
Urii455 <zhirnowu@yandex.ru>
2026-04-05 18:53:49 +0300

    делаю потехоньку

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, url_for, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
+
+list1 = ['Cold Snap', 'Ghost Walk', 'Ice Wall', 'EMP', 'Tornado', 
+         'Alacrity', 'Sun Strike', 'Forge Spirit', 'Chaos Meteor', 'Deafening Blast']
+shuffled = list1[:]
+random.shuffle(shuffled)
 
 
 if __name__ == '__main__':
